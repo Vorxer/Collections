c73ddaf839018946180562ea2aece167eb009383
Dinura <dinuras@gmail.com>
2020-01-22 07:47:05 +0530

    Second Commit

--- src/Application.java
+++ src/Application.java
@@ -1,21 +1,25 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Application {
 
     public static void main(String[] args) {
-        ArrayList<Player> Players = new ArrayList<Player>();
+        Map<Integer,Player> Players = new HashMap<Integer,Player>();
 
-        Players.add(new Player("Beaty", 87));
-        Players.add(new Player("Lutjens", 45));
-        Players.add(new Player("Isoroku", 50));
-        Players.add(new Player("Halsey", 70));
-        Players.add(new Player("Nimitz", 85));
+        Players.put(0, new Player("Nimitz", 85));
+        Players.put(1, new Player("Beaty", 87));
+        Players.put(2, new Player("Lutjens", 45));
+        Players.put(3, new Player("Isoroku", 50));
+        Players.put(4, new Player("Halsey", 70));
 
-        ArrayList<Player> sortedPlayers = Players.stream().filter(Player -> Player.getHP() >= 60).collect(Collectors.toList());
+
+        Map<Integer,Player> sortedPlayers = Players.entrySet().stream().filter(map -> map.getValue().getHP() >= 60).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));//x -> Player.getHP() >= 60)
                 //new ArrayList<Player>((Collections.sort(Players,new ComparePlayers())).stream().collect());
+        sortedPlayers.entrySet().forEach(entry->{
+            System.out.println(entry.getKey() + " " + entry.getValue().getHP());
+        });
+
+        List<Player> sortedList = new ArrayList<Player>(sortedPlayers.values());
     }
 
 
